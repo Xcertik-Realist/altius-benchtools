124931a54c2c45efcb46cb670032553be12f402f
HarryGremlins <importwyf@altiuslabs.xyz>
2025-02-12 15:44:13 +0800

    [feat] split features

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 pub mod constants;
 pub mod profiler;
+#[cfg(feature = "generator")]
 pub mod transaction_generator;
+#[cfg(feature = "generator")]
 pub use transaction_generator::TransactionGenerator;
